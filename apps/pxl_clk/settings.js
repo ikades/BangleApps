c6c439b281164f889dd20ce5f0ea4b8f92a22d6e
ikades <49671784+ikades@users.noreply.github.com>
2024-03-12 11:42:07 +0100

    Update settings.js

--- apps/pxl_clk/settings.js
+++ apps/pxl_clk/settings.js
@@ -2,7 +2,7 @@
   var FILE = "pxl_clk.json";
 
   // Initialize with default settings...
-  let s = {'bg': '#0ff', 'color': 'Cyan', 'showlock':true};
+  let s = {'bg': '#0ff', 'color': 'Cyan', 'd_or_i':'date', 'showlock':true};
 
   const storage = require('Storage');
   let settings = storage.readJSON(FILE, 1) || s;
@@ -18,6 +18,7 @@
 
   var color_options = ['Green','Orange','Cyan','Purple','Red','Blue'];
   var bg_code = ['#0f0','#ff0','#0ff','#f0f','#f00','#00f'];
+  var doir = ['date','3rd info'];
 
   // Show the menu
   E.showMenu({
@@ -30,6 +31,15 @@
       onchange: v => {
         s.color = color_options[v];
         s.bg = bg_code[v];
+        writeSettings();
+      }
+    },
+    /*LANG*/'Date or info': {
+      value: 0 | doir.indexOf(s.d_or_i),
+      min: 0, max: 1,
+      format: v => doir[v],
+      onchange: v => {
+        s.d_or_i = doir[v];
         writeSettings();
       }
     },
